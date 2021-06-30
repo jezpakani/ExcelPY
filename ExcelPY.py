9ea2db596cb9882b481a34cff0af1ce8100ee86a
jezpakani <jezpakani@protonmail.com>
2021-06-29 22:21:21 -0400

    Generating test data works in all files.

--- ExcelPY.py
+++ ExcelPY.py
@@ -2,41 +2,48 @@ import datetime
 from openpyxl import load_workbook, Workbook
 from os import system, name
 from colorama import init, Fore
-from random import randint
-from datetime import timedelta, date
+
+
+# from random import randint
+# from datetime import timedelta, date
 
 
 class ExcelPY:
-    def __init__(self, infile_name, outfile_name):
+    def __init__(self):
         """ Initialize our class and get things ready. """
         self.start_time = datetime.datetime.now()  # start timer before first operation
         self.end_time = datetime.datetime.now()  # end timer after last operation
         self.execution_time = datetime.datetime.now()  # results of our timer
-        self.infile_name = infile_name  # name of input file (data dump)
-        self.outfile_name = outfile_name  # name of output file (target excel spreadsheet)
-        self.infile = Workbook()  # file handle for input file
-        self.outfile = Workbook()  # file handle for output file
-        self.test_data_row_count = 1000  # how many rows of test data we will be creating
 
-        if not self.open_files():
-            exit()
+        # filenames for our workbooks (spreadsheets)
+        self.fn_alm = 'dump-alm.xlsx'
+        self.fn_defect = 'dump-defects.xlsx'
+        self.fn_enhancement = 'dump-enhancements.xlsx'
+        self.fn_incident = 'dump-incidents.xlsx'
+        self.fn_destination = 'cardinal-health-data-tracking.xlsx'
+
+        # workbook handles
+        self.wb_alm = Workbook()
+        self.wb_defect = Workbook()
+        self.wb_enhancement = Workbook()
+        self.wb_incident = Workbook()
+        self.wb_destination = Workbook()
+
+        self.test_data_row_count = 500  # how many rows of test data we will be creating
 
     def __del__(self):
         """ Perform cleanup operations when we destroy our class instance. """
         self.close_files()
 
-    def open_files(self):
+    def open_workbooks(self):
         """ Open files needed to perform our processes. """
         try:
-            self.infile = load_workbook(self.infile_name)
+            self.wb_alm = load_workbook(self.fn_alm)
+            self.wb_defect = load_workbook(self.fn_defect)
+            self.wb_enhancement = load_workbook(self.fn_enhancement)
+            self.wb_incident = load_workbook(self.fn_incident)
+            self.wb_destination = load_workbook(self.fn_destination)
         except Exception as e:
-            error(str(e))
-            return False
-
-        try:
-            self.outfile = load_workbook(self.outfile_name)
-        except Exception as e:
-            self.close_files()
             error(str(e))
             return False
 
@@ -44,46 +51,48 @@ class ExcelPY:
 
     def close_files(self):
         """ Close all of the files that we used. """
-        self.infile.close()
-        self.outfile.close()
+        self.wb_alm.close()
+        self.wb_defect.close()
+        self.wb_enhancement.close()
+        self.wb_incident.close()
+        self.wb_destination.close()
 
-    def generate_data_dump(self):
+    def generate_test_data(self):
         """ Populate our input file with random test data """
-        # TODO: Add logic to this method to add created data to the correct tab in our output file.
         message('Generating input file with random values')
-        self.is_not_used()
-        column_headers = ('Type Number', 'Opened', 'Short Description', 'Impact', 'Priority',
-                          'Severity', 'Status', 'Opened By', 'Assigned To', 'Incident Status',
-                          'SLA Due', 'Estimated Hours', 'Assignment Group')
-        data_types = ('INC', 'DFCT', 'ENHC', 'ALM')
-        levels = ('1 - Low', '2 - Medium', '3 - High')
-        status = ('Open', 'Closed')
-        people = ('Pete Skeebo', 'Sam Sausagehead', 'Mrs. Buttersworth', 'Jax Vonhapsburg')
+        if not self.open_workbooks():
+            exit()
 
-        sheet = self.infile.active  # grab the first sheet
-        sheet.title = 'Dump'
-        sheet.delete_rows(1, sheet.max_row + 1)  # now delete all the rows as we are starting from scratch
+        # populate our data dump input files
+        self.populate_sheet(self.wb_alm, self.wb_alm.active, self.fn_alm, 'alm')
+        self.populate_sheet(self.wb_defect, self.wb_defect.active, self.fn_defect, 'dfc')
+        self.populate_sheet(self.wb_incident, self.wb_incident.active, self.fn_incident, 'inc')
+        self.populate_sheet(self.wb_enhancement, self.wb_enhancement.active, self.fn_enhancement, 'enh')
 
-        for x in range(0, len(column_headers)):  # create our column headers
-            sheet.cell(row=1, column=x + 1).value = column_headers[x]
+        # now populate our main reporting file with data that perfectly matches our dumps
+        self.populate_sheet(self.wb_destination, self.wb_destination['ALM Defects'],
+                            self.fn_destination, 'alm')
+        self.populate_sheet(self.wb_destination, self.wb_destination['Hypercare Defects'],
+                            self.fn_destination, 'dfc')
+        self.populate_sheet(self.wb_destination, self.wb_destination['Hypercare Incidents'],
+                            self.fn_destination, 'inc')
+        self.populate_sheet(self.wb_destination, self.wb_destination['Hypercare Enhancements'],
+                            self.fn_destination, 'enh')
 
-        for x in range(2, self.test_data_row_count):  # now lets generate random cell data
-            sheet.cell(row=x, column=1).value = data_types[randint(0, len(data_types) - 1)] + str(randint(10000, 99999))
-            sheet.cell(row=x, column=2).value = date.today() + timedelta(days=randint(1, 10))
-            sheet.cell(row=x, column=3).value = 'Short Description #' + str(x)
-            sheet.cell(row=x, column=4).value = levels[randint(0, len(levels) - 1)]
-            sheet.cell(row=x, column=5).value = levels[randint(0, len(levels) - 1)]
-            sheet.cell(row=x, column=6).value = levels[randint(0, len(levels) - 1)]
-            sheet.cell(row=x, column=7).value = status[randint(0, len(status) - 1)]
-            sheet.cell(row=x, column=8).value = people[randint(0, len(people) - 1)]
-            sheet.cell(row=x, column=9).value = people[randint(0, len(people) - 1)]
-            sheet.cell(row=x, column=10).value = status[randint(0, len(status) - 1)]
-            sheet.cell(row=x, column=11).value = date.today() + timedelta(days=randint(1, 10))
-            sheet.cell(row=x, column=12).value = randint(1, 10)
-            sheet.cell(row=x, column=13).value = 'Assignment Group #' + str(randint(1, 10))
-
-        self.infile.save(self.infile_name)
         message('Completed generating input file')
+
+    def populate_sheet(self, wb, ws, fn, tag):
+        ws.delete_rows(2, ws.max_row + 1)
+
+        for x in range(2, self.test_data_row_count + 1):  # now lets generate some cell data
+            for y in range(1, ws.max_column + 1):  # worksheet columns are not zero-based so add 1
+                ws.cell(row=x, column=y).value = '[{}]:{}:{}'.format(tag, x, y)
+
+        wb.save(fn)
+        # ws_hdr = list()
+        # get a list of column headers so we can use them for searching
+        # for row in ws.iter_rows(min_row=1, max_col=ws.max_column, max_row=1, values_only=True):
+        #     ws_hdr.append(row)
 
     def get_execution_time(self):
         """ Display the results of our execution timer. """
@@ -113,21 +122,26 @@ def clear_screen():
 
 def message(value=''):
     """ Format general messages including attributes. """
-    print(Fore.YELLOW + '+++ ' + value + ' +++')
+    print(Fore.GREEN + '+++ ' + value + ' +++')
 
 
 def error(value=''):
     """ Format error messages including attributes. """
-    print(Fore.RED + '+++ ' + value + ' +++')
+    print(Fore.RED + '!!! ' + value + ' !!!')
+
+
+def warning(value=''):
+    """ Format warning messages including attributes. """
+    print(Fore.YELLOW + '!!! ' + value + ' !!!')
 
 
 if __name__ == '__main__':
     clear_screen()
     init(autoreset=True)
     message('Initiating Process')
-    xc = ExcelPY('input_data.xlsx', 'output_data.xlsx')
+    xc = ExcelPY()
     xc.start_timer()
-    xc.generate_data_dump()
+    xc.generate_test_data()
     xc.stop_timer()
     xc.get_execution_time()
     del xc
